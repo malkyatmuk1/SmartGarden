20490177597ded9660472d69f616c6b023dd01b9
danisini <dani.levski00@gmail.com>
2019-05-07 13:47:07 +0300

    vsichko

--- app/src/main/java/com/example/malkyatmuk/smartgarden/Add_Card.java
+++ app/src/main/java/com/example/malkyatmuk/smartgarden/Add_Card.java
@@ -1,6 +1,8 @@
 package com.example.malkyatmuk.smartgarden;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
